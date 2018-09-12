d2d66308122eca8fd28485ac397b89df8602165b
Pavel Golenkov <pgolenkov@gmail.com>
2018-09-12 16:10:07 +0500

    Add task2 (area of triangle) for lesson2

---
+++ lesson2/triangle_area.rb
@@ -0,0 +1,6 @@
+puts "Введите основание треугольника:"
+base = gets.to_f
+puts "Введите высоту треугольника:"
+height = gets.to_f
+area = 0.5 * base * height
+puts "Площадь треугольника равна: #{area}"
