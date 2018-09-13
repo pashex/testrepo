45a3aea279c43673f4fd8b7f9f4770d44d9b1dc2
Pavel Golenkov <pgolenkov@gmail.com>
2018-09-13 10:49:15 +0500

    Refactor task (equation) of lesson2

--- lesson2/equation.rb
+++ lesson2/equation.rb
@@ -1,28 +1,23 @@
-k = {}
-%w(a b c).each do |coeff_name|
-  puts "Введите коэффициент '#{coeff_name}' уравнения:"
-  k[coeff_name] = gets.to_f
+def solve_equation(a, b, c)
+  return puts("Выражение не является уравнением. Ответа нет") if a == 0 && b == 0
+  return puts("Уравнение не является квадратным. x=#{-c / b}") if a == 0
+
+  d = b ** 2 - 4 * a * c
+  return puts("Корней уравнения нет. D=#{d}") if d < 0
+  return puts("D=0, корень один x=#{-b / (2 * a)}") if d == 0
+
+  sqrt_d = Math.sqrt(d)
+  x1 = (-b + sqrt_d) / (2 * a)
+  x2 = (-b - sqrt_d) / (2 * a)
+  puts "D=#{d}, корни уравнения x1=#{x1}; x2=#{x2}"
 end
 
-if k['a'] == 0
-  if k['b'] == 0
-    puts "Выражение не является уравнением. Ответа нет"
-  else
-    x = -k['c'] / k['b']
-    puts "Уравнение не является квадратным. x=#{x}"
-  end
-else
-  discriminant = k['b'] ** 2 - 4 * k['a'] * k['c']
-  if discriminant < 0
-    puts "Корней уравнения нет. D=#{discriminant}"
-  elsif discriminant == 0
-    x = - k['b'] / (2 * k['a'])
-    puts "D=0, корень один x=#{x}"
-  else
-    sqrt_discriminant = Math.sqrt(discriminant)
-    x1 = (- k['b'] + sqrt_discriminant) / (2 * k['a'])
-    x2 = (- k['b'] - sqrt_discriminant) / (2 * k['a'])
-    puts "D=#{discriminant}, корни уравнения x1=#{x1}; x2=#{x2}"
-  end
+ks = []
+%w(a b c).each do |coeff_name|
+  puts "Введите коэффициент '#{coeff_name}' уравнения:"
+  ks << gets.to_f
 end
+solve_equation(*ks)
+
+
 
