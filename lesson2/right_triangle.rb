f3a97b7fa15120bf42839c1386294b758e3a9c8e
Pavel Golenkov <pgolenkov@gmail.com>
2018-09-13 09:43:00 +0500

    Refactor triangle sides task

--- lesson2/right_triangle.rb
+++ lesson2/right_triangle.rb
@@ -1,25 +1,35 @@
+def triangle?(a, b, c)
+  a + b > c && b + c > a && a + c > b
+end
+
+def right?(*sides)
+  max_side, max_side_index = sides.each_with_index.max
+  sides.delete_at max_side_index
+  sides[0] ** 2 + sides[1] ** 2 == max_side ** 2
+end
+
+def isosceles?(a, b, c)
+  [a, b, c].uniq.length == 2
+end
+
+def equilateral?(sides)
+  sides.uniq.one?
+end
+
+def test_triangle(sides)
+  return puts("Это не треугольник") unless triangle?(*sides)
+  return puts("Треугольник прямоугольный") if right?(*sides)
+  return puts("Треугольник равносторонний") if equilateral?(sides)
+  return puts("Треугольник равнобедренный") if isosceles?(*sides)
+  puts "Треугольник не является ни прямоугольным, ни равнобедренным"
+end
+
 sides = []
 3.times.each do |n|
   puts "Введите #{n + 1} сторону треугольника:"
   sides << gets.to_f
 end
 
-max_side, max_side_index = sides.each_with_index.max
-puts "Самая большая - #{max_side_index + 1} сторона: #{max_side}"
+test_triangle(sides)
 
-sides.delete_at(max_side_index)
-
-if sides[0] ** 2 + sides[1] ** 2 == max_side ** 2
-  puts "Треугольник прямоугольный"
-else
-  puts "Треугольник не прямоугольный"
-end
-
-if sides[0] == sides[1]
-  if sides[1] == max_side
-    puts "Треугольник равносторонний"
-  else
-    puts "Треугольник равнобедренный"
-  end
-end
 
