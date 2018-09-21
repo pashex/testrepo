6794a0535de2168a6ceec23895e27cf98c5fe970
Pavel Golenkov <pgolenkov@gmail.com>
2018-09-21 09:00:41 +0500

    Refactor tasks for lesson3

--- lesson3/task2.rb
+++ lesson3/task2.rb
@@ -1,2 +1,2 @@
-my_array = (10..100).map { |number| number if number % 5 == 0 }.compact
+my_array = (10..100).select { |number| number % 5 == 0 }
 puts my_array.inspect
--- lesson3/task5.rb
+++ lesson3/task5.rb
@@ -1,22 +1,20 @@
 def leap_year?(year)
-  return unless year % 4 == 0
-  return if year % 100 == 0 && year % 400 != 0
-  true
+  year % 4 == 0 && year % 100 != 0 || year % 400 == 0
 end
 
-month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 day, month, year = ['Введите день', 'Введите месяц', 'Введите год'].map do |question|
   puts question
   gets.chomp.to_i
 end
 
-month_days[1] = 29 if leap_year?(year)
+month_days[2] = 29 if leap_year?(year)
 
-if day <= 0 || month <= 0 || month_days[month - 1].to_i < day
+if day <= 0 || month <= 0 || month_days[month].to_i < day
   puts "Ошибка даты"
 else
-  day_number = (1...month).map { |m| month_days[m - 1] }.inject(0, :+) + day
+  day_number = month_days[1...month].inject(0, :+) + day
   puts "Порядковый номер дня с начала года: #{day_number}"
 end
 
